7dfe36ccbb0dce3d24d7fbdd3e549bda5efc5a21
Grisha Mokin <131665428+grishaqq@users.noreply.github.com>
2024-09-02 15:17:18 +0200

    small comment update evaluate_order_player.py

--- evaluate_order_player.py
+++ evaluate_order_player.py
@@ -9,8 +9,6 @@ class EvaluateOrderPlayer(Player):
         target = battle.all_active_pokemons[1]
         available_orders = battle.available_moves + battle.available_switches
         if available_orders:
-            # for order in available_orders:
-            #    print(evaluate_order(battle, user, target, order), order)
             best_move = max(
                 available_orders,
                 key=lambda order: evaluate_order(battle, user, target, order),
